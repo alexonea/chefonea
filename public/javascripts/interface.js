81c05e1153ce574ca2dec0c20448fef4eb9b08b8
Alexandru Onea <onea.alex@gmail.com>
2013-04-24 01:54:55 +0300

    all files

---
+++ public/javascripts/interface.js
@@ -0,0 +1,22 @@
+var dragEnabled = false;
+var dragElement = null;
+
+// $('.matrix').draggable();
+
+// $('.matrix').mousedown(function (e) {
+// 	$('.matrix').draggable('enable');
+// 	dragEnabled = true;
+// });
+
+// $(document).mouseup(function (e) {
+// 	if (dragEnabled == true) {
+// 		$('.matrix').draggable('disable');
+// 		dragEnabled = false;
+// 	}
+// });
+
+// $(document).mousemove(function (e) {
+// 	if (dragEnabled) {
+// 		$('.matrix').draggable('enable');
+// 	}
+// });
