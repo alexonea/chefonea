0138a567fdef10f8e6133a2dcb60c126f78ccb2f
Alexandru Onea <onea.alex@gmail.com>
2013-05-11 17:41:48 +0100

    implemented facebook login

--- app.js
+++ app.js
@@ -4,10 +4,13 @@
  */
 
 var express = require('express')
+  , passport = require('passport')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
+
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 
 routes.forum = require('./routes/forum');
@@ -22,9 +25,46 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.favicon('favicon.png'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(process.env.FACEBOOK_APP_ID);
+console.log(process.env.FACEBOOK_APP_SECRET);
+console.log(process.env.CALLBACK_URL);
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+  },
+  function (accessToken, refreshToken, profile, done) {
+    var Database = require('lib/database.js');
+    var db = new Database();
+
+    db.findUser(profile.id, function (err, user) {
+      if (err) { return done(err); }
+      if (user.length === 0) {
+        db.addUser(profile, function () {
+          var result = new Object();
+          result.id = profile.id;
+          done(null, result);
+        });
+      } else
+        done(null, user[0]);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 // development only
 if ('development' == app.get('env')) {
@@ -34,6 +74,11 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/forum', routes.forum);
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/' }));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
