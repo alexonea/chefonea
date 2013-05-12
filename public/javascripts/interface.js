360663fb08fca0eb3ae6b5682458af9c872dc00f
Alexandru Onea <onea.alex@gmail.com>
2013-05-13 00:18:27 +0100

    revert

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -575,7 +575,7 @@ $('#load').on('click', function(e) {
 	$.ajax({
 		type: 'GET',
 		url: '/load',
-		data: JSON.stringify({uid: uid}),
+		data: {uid: uid},
 		dataType: 'json',
 		traditional: true,
 		contentType: "application/json; charset=utf-8",
